69bc057df894fa07539f941fe372e5a0de8c1e6b
pe4nuttt <tienanh1512@gmail.com>
2024-09-27 07:14:08 +0700

    [UPDATE] tiptap version & Documnet

---
+++ components/pages/document/DocumentEditor.vue
@@ -0,0 +1,130 @@
+<template>
+  <div>
+    <EditorContent class="h-full" :editor="tiptapEditor" />
+    <template v-if="tiptapEditor">
+      <TextBubbleMenu :editor="tiptapEditor" />
+    </template>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { useEditor, EditorContent, Editor } from '@tiptap/vue-3';
+import type { Editor as TypeEditor } from '@tiptap/vue-3';
+import StarterKit from '@tiptap/starter-kit';
+import * as Y from 'yjs';
+import { HocuspocusProvider } from '@hocuspocus/provider';
+import { ExtensionKit } from '~/lib/tiptap/extension-kit';
+import TextBubbleMenu from '@/components/app/tiptap/TextBubbleMenu/TextBubbleMenu.vue';
+import type { ShallowRef } from 'vue';
+
+// Data
+const editor = useEditor({
+  content: `
+    <p>I’m running Tiptap with Vue.js. 🎉</p>
+    <div data-type="pageNode"></div>
+  `,
+  extensions: [...ExtensionKit],
+  editorProps: {
+    attributes: {
+      class: 'min-h-full',
+    },
+  },
+});
+
+// console.log('[TEST]', editor);
+
+// const editor = useTiptapEditor({
+//   ydoc,
+//   provider,
+// });
+const wsConnection = new WebSocket('ws://127.0.0.1:8080/sync');
+const ydoc = new Y.Doc();
+const provider = new HocuspocusProvider({
+  name: 'document_019b1c45-b69c-4d67-ad5d-a54dd64cf374',
+  url: wsConnection.url,
+  document: ydoc,
+});
+
+// const provider = ref<HocuspocusProvider>();
+// const tiptapEditor = ref<ShallowRef<Editor | undefined>>();
+let tiptapEditor = shallowRef<Editor | undefined>();
+
+// Computed
+// const editor = computed(() => {
+//   if (provider?.value) {
+//     const { editor: tmpEditor } = useTiptapEditor({
+//       ydoc: ydoc,
+//       provider: provider.value,
+//     });
+
+//     console.log('[COMPUTED] editor', tmpEditor);
+
+//     return tmpEditor;
+//   }
+//   return null;
+// });
+
+// watch(
+//   () => [provider, ydoc],
+//   () => {
+//     if (provider.value) {
+//       // const { editor } = useTiptapEditor({
+//       //   ydoc: ydoc.value,
+//       //   provider: provider.value as HocuspocusProvider,
+//       // });
+//     }
+//     const editor = useEditor({
+//       content: '<p>I’m running Tiptap with Vue.js. 🎉</p>',
+//       extensions: [...ExtensionKit],
+//       editorProps: {
+//         attributes: {
+//           class: 'min-h-full',
+//         },
+//       },
+//     });
+
+//     // tiptapEditor.value = editor;
+//     console.log('[WATCH] Editor', editor);
+//   },
+//   {
+//     immediate: true,
+//     deep: true,
+//   },
+// );
+
+// Methods
+// const initHocuspocusProvider = async () => {
+//   try {
+//     const wsConnection = new WebSocket('ws://127.0.0.1:3000/sync');
+//     provider.value = new HocuspocusProvider({
+//       name: 'document.name',
+//       url: wsConnection.url,
+//       document: ydoc.value,
+//     });
+//   } catch (error) {
+//     console.log('[ERR] init provider', error);
+//   }
+
+//   console.log('[INIT]', provider.value);
+// };
+
+// onBeforeMount(() => {
+//   console.log(123456);
+//   initHocuspocusProvider();
+// });
+
+onMounted(async () => {
+  await nextTick();
+  const { editor } = useTiptapEditor({
+    ydoc: ydoc,
+    provider: provider,
+  });
+  tiptapEditor.value = editor;
+
+  console.log('[MOUNTED]', editor);
+
+  // initHocuspocusProvider();
+});
+</script>
+
+<style scoped lang="scss"></style>
---
+++ components/pages/document/DocumentHeaderInfo.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <!-- Document Title -->
+    <div>
+      <h1
+        contenteditable
+        class="text-3xl font-semibold !outline-none"
+        @input="onInputDocumentTitle"
+      >
+        {{ documentTitle }}
+      </h1>
+    </div>
+
+    <div></div>
+  </div>
+</template>
+
+<script setup lang="ts">
+const documentTitle = ref('Document Title');
+
+const onInputDocumentTitle = (e: Event) => {
+  console.log((e.target as HTMLInputElement).value);
+};
+</script>
+
+<style scoped></style>
